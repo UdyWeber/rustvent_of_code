9839175c2a00b8503abf65112a0ebc4a30b49720
Jolestest Windows <tuyweber@gmail.com>
2023-11-03 21:05:16 -0300

    Resolved day four

---
+++ src/day_four/day_four.rs
@@ -0,0 +1,66 @@
+use std::fs;
+
+#[derive(Debug)]
+struct Section {
+    start: u32,
+    end: u32,
+}
+
+
+impl Section {
+    pub fn new(from_string: &str) -> Self {
+        let (start, end) = from_string.split_once("-").unwrap();
+
+        Self {
+            start: start.parse::<u32>().unwrap(),
+            end: end.parse::<u32>().unwrap(),
+        }
+    }
+
+    pub fn contains_or_is_contained(&self, s: &Section) -> bool {
+        let (contains_range, is_contained_range)
+            = ((self.start..=self.end), (s.start..=s.end));
+
+        contains_range.contains(&s.start)
+            || contains_range.contains(&s.end)
+            || is_contained_range.contains(&self.start)
+            || is_contained_range.contains(&self.end)
+    }
+
+    pub fn merge(&self, s: Section) -> Self {
+        let smaller_start = match self.start > s.start {
+            true => { s.start }
+            false => { self.start }
+        };
+
+        let greater_end = match self.end < s.end {
+            true => { s.end }
+            false => { self.end }
+        };
+
+        Section {
+            start: smaller_start,
+            end: greater_end,
+        }
+    }
+}
+
+pub fn day_four_resolution() {
+    let file_read = fs::read_to_string("src/day_four/my_input.txt").unwrap();
+
+    let overlapped_sections = file_read
+        .lines()
+        .map(|line| {
+            let (first_section, second_section) = line.split_once(",").unwrap();
+            (Section::new(first_section), Section::new(second_section))
+        })
+        .fold(0, |mut sum, (first_section, second_section)| {
+            if first_section.contains_or_is_contained(&second_section) {
+                sum += 1;
+            }
+
+            return sum;
+        });
+
+    println!("{overlapped_sections}")
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod day_one;
 mod day_two;
 
 mod day_three;
+mod day_four;
 
 fn main() {
-    println!("In day one the maximum calories brought by the elves was {}", day_one::day_one::day_one_resolution());
-    println!("Game points: {}", day_two::day_two::day_two_resolution());
-    println!("Sum of items priority: {}", day_three::day_three::day_three_resolution());
+    // println!("In day one the maximum calories brought by the elves was {}", day_one::day_one::day_one_resolution());
+    // println!("Game points: {}", day_two::day_two::day_two_resolution());
+    // println!("Sum of items priority: {}", day_three::day_three::day_three_resolution());
+    day_four::day_four::day_four_resolution();
 }
